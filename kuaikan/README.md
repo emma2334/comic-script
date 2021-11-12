ab865379060123a1709d6d255d10e649362cb317
Emma <emma2334@gmail.com>
2021-11-12 15:51:27 +0800

    Gloss over locked chapters

---
+++ kuaikan/README.md
@@ -0,0 +1,7 @@
+# 快看連續打包
+
+獻給那些有收藏癖、喜歡看漫畫但沒時間或喜歡攢著一口氣看完的小姐妹們
+
+[![Install](http://i.imgur.com/NHH6Q.png)](https://github.com/emma2334/comic-script/raw/master/kuaikan/kuaikan-packing.user.js)
+
+請先安裝 [Tampermonkey](http://tampermonkey.net/)（Safari、Chrome）或 [GreaseMonkey](https://addons.mozilla.org/zh-tw/firefox/addon/greasemonkey/)（Firefox）後再[加入腳本](https://github.com/emma2334/comic-script/raw/master/kuaikan/kuaikan-packing.user.js)。
--- kuaikan/kuaikan-packing.user.js
+++ kuaikan/kuaikan-packing.user.js
@@ -3,20 +3,29 @@
 // @namespace    https://emma2334.github.io
 // @version      0.1
 // @description  連續打包快看漫畫成 zip 檔
-// @author       You
-// @match        https://www.kuaikanmanhua.com/web/comic/*
+// @author       Emma (emma2334)
+// @homepage     https://emma2334.github.io
+// @match        https://www.kuaikanmanhua.com/web/*
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.5.0/jszip.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip-utils/0.1.0/jszip-utils.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js
 // @icon         https://www.google.com/s2/favicons?domain=kuaikanmanhua.com
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/emma2334/comic-script/master/kuaikan/kuaikan-packing.user.js
+// @downloadURL  https://github.com/emma2334/comic-script/raw/master/kuaikan/kuaikan-packing.user.js
 // ==/UserScript==
 
 document.onreadystatechange = function() {
   'use strict';
   if(document.readyState !== 'complete') return
+
+  if(window.location.pathname.match('topic/')) {
+    $('.TopicItem:has(.lockedIcon)').css({
+      'background-color': '#eaeaea',
+      opacity: 0.3
+    })
+    return
+  }
 
   function urlToPromise(url) {
     return new Promise(function(resolve, reject) {
@@ -27,6 +36,8 @@ document.onreadystatechange = function() {
   }
   var img = $('.imgList img').toArray().map(e => $(e).attr('data-src'));
 
+  if(!img.length) return
+
   var zip = new JSZip();
 
   img.forEach((e, i) => {
@@ -34,14 +45,14 @@ document.onreadystatechange = function() {
   });
   zip.generateAsync({type:"blob"})
   .then(function(content) {
-    const title = $('.listBox li[style] a').html()
-    const index = $('.listBox li:not(.title) a').toArray().reverse().map((e, i) => $(e).html()).indexOf(title) + 1
-    saveAs(content, `#${index} ${title}.zip`);
+    const title = $('.listBox li[style] a').text()
+    const index = $('.listBox li:not(.title) a').toArray().reverse().map((e, i) => $(e).text()).indexOf(title) + 1
+    saveAs(content, `${$('.listBox li.title a').text()} - #${index} ${title}.zip`);
 
     if($('.AdjacentChapters.topNav .btn a')){
       setTimeout(() => {
         window.location.href = $('.AdjacentChapters.topNav .btn a').eq(1).attr('href')
-      }, 3000)
+      }, 1000)
     }
   }, function(error) {
     alert(error);
